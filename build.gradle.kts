e287dc4fb97813d08b13988bcca1549466cb1f63
Elvis Oliveira <elvis.olv@gmail.com>
2026-08-27 20:10:45 +0200

    Bump to 1.2 (versionCode 3)

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "poc.ringclick"
         minSdk = 31
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
     }
 
     compileOptions {
